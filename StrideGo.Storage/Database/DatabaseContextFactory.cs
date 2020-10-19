051294800b58cde00db65f1123ed39d964d55293
Ynel Basa <marinelbasa@gmail.com>
2020-10-20 09:37:25 +1300

    Create database and setup migrations

---
+++ StrideGo.Storage/Database/DatabaseContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrideGo.Storage.Database
+{
+    // For migration purposes, update the connection string accordingly
+    public class DatabaseContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
+    {
+        public DatabaseContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+            optionsBuilder.UseSqlServer("Server=localhost;Initial Catalog=StrideGo.DatabaseDev;MultipleActiveResultSets=False;Integrated Security=True;Connection Timeout=60;");
+
+            return new DatabaseContext(optionsBuilder.Options);
+        }
+    }
+}
