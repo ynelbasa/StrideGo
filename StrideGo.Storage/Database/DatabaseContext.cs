c40143673c0097b4ab15a0f0558dbeeee23277e5
Ynel Basa <marinelbasa@gmail.com>
2020-10-20 08:28:57 +1300

    Add default constructor for db context to apply configuration

--- StrideGo.Storage/Database/DatabaseContext.cs
+++ StrideGo.Storage/Database/DatabaseContext.cs
@@ -5,5 +5,8 @@ namespace StrideGo.Storage
 {
     public class DatabaseContext : DbContext, IDatabaseContext
     {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
     }
 }
