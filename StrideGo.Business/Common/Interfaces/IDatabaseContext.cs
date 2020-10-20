87df26a6429f462693c8344ca9bce876bf494e11
Ynel Basa <marinelbasa@gmail.com>
2020-10-20 18:11:48 +1300

    Add DbSet to the interface

--- StrideGo.Business/Common/Interfaces/IDatabaseContext.cs
+++ StrideGo.Business/Common/Interfaces/IDatabaseContext.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using StrideGo.Domain.Entities;
 
 namespace StrideGo.Business.Common.Interfaces
 {
     public interface IDatabaseContext
     {
+        DbSet<User> Users { get; set; }
+        DbSet<UserLogin> UserLogins { get; set; }
+        DbSet<Question> Questions { get; set; }
+        DbSet<QuestionCategory> QuestionCategories { get; set; }
+        DbSet<QuestionVote> QuestionVotes { get; set; }
+        DbSet<Answer> Answers { get; set; }
     }
 }
