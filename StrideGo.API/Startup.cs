be80f21486d0a27c6e8905bd0c8ffb94ece70b5e
Ynel Basa <marinelbasa@gmail.com>
2020-10-20 18:10:33 +1300

    Add MediatR and AutoMapper to StrideGo.API, move service dependencies

--- StrideGo.API/Startup.cs
+++ StrideGo.API/Startup.cs
@@ -1,18 +1,14 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using StrideGo.Business;
+using StrideGo.Storage;
 
 namespace StrideGo.API
 {
@@ -28,6 +24,9 @@ namespace StrideGo.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddStorage(Configuration);
+            services.AddBusiness();
+
             services.AddControllers();
 
             // Register the Swagger generator, defining 1 or more Swagger documents
---
+++ StrideGo.Business/ServiceCollectionExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StrideGo.Business
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddBusiness(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
