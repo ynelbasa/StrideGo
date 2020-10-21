12a004d84dbd2d07d3d05106a672ac056813bb12
Ynel Basa <marinelbasa@gmail.com>
2020-10-22 09:38:28 +1300

    Add question and user entity relationship to QuestionVote

--- StrideGo.Domain/Entities/QuestionVote.cs
+++ StrideGo.Domain/Entities/QuestionVote.cs
@@ -9,7 +9,11 @@ namespace StrideGo.Domain.Entities
     public class QuestionVote
     {
         public int Id { get; set; }
+        public int QuestionId { get; set; }
         public int UserId { get; set; }
         public bool IsUpvote { get; set; }
+
+        public Question Question { get; set; }
+        public User User { get; set; }
     }
 }
---
+++ StrideGo.Storage/Migrations/20201021203657_AddForeignEntitiesToQuestionVoteTable.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StrideGo.Storage.Migrations
+{
+    public partial class AddForeignEntitiesToQuestionVoteTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuestionId",
+                schema: "StrideGo",
+                table: "QuestionVote",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionVote_QuestionId",
+                schema: "StrideGo",
+                table: "QuestionVote",
+                column: "QuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionVote_UserId",
+                schema: "StrideGo",
+                table: "QuestionVote",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuestionVote_Question_QuestionId",
+                schema: "StrideGo",
+                table: "QuestionVote",
+                column: "QuestionId",
+                principalSchema: "StrideGo",
+                principalTable: "Question",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuestionVote_User_UserId",
+                schema: "StrideGo",
+                table: "QuestionVote",
+                column: "UserId",
+                principalSchema: "StrideGo",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuestionVote_Question_QuestionId",
+                schema: "StrideGo",
+                table: "QuestionVote");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuestionVote_User_UserId",
+                schema: "StrideGo",
+                table: "QuestionVote");
+
+            migrationBuilder.DropIndex(
+                name: "IX_QuestionVote_QuestionId",
+                schema: "StrideGo",
+                table: "QuestionVote");
+
+            migrationBuilder.DropIndex(
+                name: "IX_QuestionVote_UserId",
+                schema: "StrideGo",
+                table: "QuestionVote");
+
+            migrationBuilder.DropColumn(
+                name: "QuestionId",
+                schema: "StrideGo",
+                table: "QuestionVote");
+        }
+    }
+}
