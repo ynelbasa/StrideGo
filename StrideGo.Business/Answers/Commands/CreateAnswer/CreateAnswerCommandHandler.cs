0947d8940ebed20ae140651bcd668cefe7b01b37
Ynel Basa <marinelbasa@gmail.com>
2020-10-22 15:03:31 +1300

    Implement query handler for adding new answer

---
+++ StrideGo.Business/Answers/Commands/CreateAnswer/CreateAnswerCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using StrideGo.Business.Common.Interfaces;
+using StrideGo.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StrideGo.Business.Answers.Commands.CreateAnswer
+{
+    public class CreateAnswerCommandHandler : IRequestHandler<CreateAnswerCommand, int>
+    {
+        private readonly IDatabaseContext _context;
+        public CreateAnswerCommandHandler(IDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateAnswerCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new Answer
+            {
+                QuestionId = request.QuestionId,
+                UserId = request.UserId,
+                Text = request.Text,
+                IsActive = true                
+            };
+
+            _context.Answers.Add(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+    }
+}
