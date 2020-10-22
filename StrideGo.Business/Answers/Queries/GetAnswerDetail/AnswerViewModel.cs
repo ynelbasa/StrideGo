99d5fb64d2e82fb6dd1e34d5d51edce158f415ea
Ynel Basa <marinelbasa@gmail.com>
2020-10-22 16:30:38 +1300

    Get answer detail query and handler, add AnswerViewModel

---
+++ StrideGo.Business/Answers/Queries/GetAnswerDetail/AnswerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using StrideGo.Business.Common.Mappings;
+using StrideGo.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrideGo.Business.Answers.Queries.GetAnswerDetail
+{
+    public class AnswerViewModel : IMapFrom<Answer>
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Answer, AnswerViewModel>();
+        }
+    }
+}
---
+++ StrideGo.Business/Answers/Queries/GetAnswerDetail/GetAnswerDetailQuery.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using StrideGo.Business.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StrideGo.Business.Answers.Queries.GetAnswerDetail
+{
+    public class GetAnswerDetailQuery : IRequest<AnswerViewModel>
+    {
+        public int Id { get; set; }
+
+        public class Handler : IRequestHandler<GetAnswerDetailQuery, AnswerViewModel>
+        {
+            private readonly IMapper _mapper;
+            private readonly IDatabaseContext _context;
+            public Handler(IMapper mapper, IDatabaseContext context)
+            {
+                _mapper = mapper;
+                _context = context;
+            }
+
+            public async Task<AnswerViewModel> Handle(GetAnswerDetailQuery request, CancellationToken cancellationToken)
+            {
+                var answer = await _context.Answers
+                    .ProjectTo<AnswerViewModel>(_mapper.ConfigurationProvider)
+                    .FirstOrDefaultAsync(q => q.Id == request.Id);
+
+                return answer;
+            }
+        }
+    }
+}
