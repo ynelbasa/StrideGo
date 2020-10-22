0e0e6e29ab3dc3be7e6b0b84da950370a3d088c3
Ynel Basa <marinelbasa@gmail.com>
2020-10-22 16:15:32 +1300

    Delete answer command and handler

---
+++ StrideGo.Business/Answers/Commands/DeleteAnswer/DeleteAnswerCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrideGo.Business.Answers.Commands.DeleteAnswer
+{
+    public class DeleteAnswerCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ StrideGo.Business/Answers/Commands/DeleteAnswer/DeleteAnswerCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using StrideGo.Business.Common.Exceptions;
+using StrideGo.Business.Common.Interfaces;
+using StrideGo.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StrideGo.Business.Answers.Commands.DeleteAnswer
+{
+    public class DeleteAnswerCommandHandler : IRequestHandler<DeleteAnswerCommand>
+    {
+        private readonly IDatabaseContext _context;
+        public DeleteAnswerCommandHandler(IDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteAnswerCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Answers.FindAsync(request.Id);
+            if (entity == null)
+            {
+                throw new EntityNotFoundException(nameof(Answer), request.Id);
+            }
+
+            entity.IsActive = false;
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
