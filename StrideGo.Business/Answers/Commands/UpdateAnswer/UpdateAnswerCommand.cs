2d6635b9703cb7ae44743a033b07ff44566fa37e
Ynel Basa <marinelbasa@gmail.com>
2020-10-22 16:07:52 +1300

    Update answer command and handler

---
+++ StrideGo.Business/Answers/Commands/UpdateAnswer/UpdateAnswerCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrideGo.Business.Answers.Commands.UpdateAnswer
+{
+    public class UpdateAnswerCommand : IRequest
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
